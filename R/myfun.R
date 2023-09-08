4bbae0f64a8ec0b1e49d06714c6a073ff4feac25
Gavin Orahood <gavin.j.orahood-1@ou.edu>
2023-09-08 14:06:47 -0500

    Init

---
+++ R/myfun.R
@@ -0,0 +1,12 @@
+#' My first function
+#'
+#' @param x A quantitative vector
+#'
+#' @return A vector of squared values plus 10
+#' @export
+#'
+#' @examples
+#' myfun(1:10)
+myfun <- function(x){
+ x^2+10
+}

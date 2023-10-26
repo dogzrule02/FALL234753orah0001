39b19d571470c708117ba47b25eb556943e94240
Gavin Orahood <gavin.j.orahood-1@ou.edu>
2023-10-25 20:38:40 -0500

    Please

---
+++ R/myCLT.R
@@ -0,0 +1,20 @@
+#' MYCLT
+#'
+#' @param n-size of sample
+#' @param iter-iterations
+#' @param a - quantity
+#' @param b -quantity
+#'
+#' @return - histogram of the distributions
+#' @export
+#'
+#' @examples
+myclt=function(n,iter,a=0,b=5){
+  y=runif(n*iter,a,b)
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  sm=apply(data,2,sum)
+  h=hist(sm,plot=FALSE)
+  hist(sm,col=rainbow(length(h$mids)),freq=FALSE,main="Distribution of the sum of uniforms")
+  curve(dnorm(x,mean=n*(a+b)/2,sd=sqrt(n*(b-a)^2/12)),add=TRUE,lwd=2,col="Blue")
+  sm
+}
